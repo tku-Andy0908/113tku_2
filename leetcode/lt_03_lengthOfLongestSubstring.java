1795d1a8e36f725fa44066d8c7173ed1debd4f32
tku-Andy0908 <412631433@o365.tku.edu.tw>
2025-09-04 01:38:10 +0800

    Update lt_03_lengthOfLongestSubstring.java

--- leetcode/lt_03_lengthOfLongestSubstring.java
+++ leetcode/lt_03_lengthOfLongestSubstring.java
@@ -7,13 +7,13 @@ class Solution {
 
         for (int right = 0; right < s.length(); right++) {
             char c = s.charAt(right);
-            // 如果字元已存在，移動左指針避免重複
             if (map.containsKey(c) && map.get(c) >= left) {
-                left = map.get(c) + 1;
+                left = map.get(c) + 1; // 更新左邊界
             }
             map.put(c, right);
             maxLen = Math.max(maxLen, right - left + 1);
         }
+
         return maxLen;
     }
 }

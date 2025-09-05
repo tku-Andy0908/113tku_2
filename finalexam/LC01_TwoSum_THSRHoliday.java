8186ade676876d0dc853e95add7c9fd292a4c8d2
tku-Andy0908 <412631433@o365.tku.edu.tw>
2025-09-05 20:15:58 +0800

    5

--- finalexam/LC01_TwoSum_THSRHoliday.java
+++ finalexam/LC01_TwoSum_THSRHoliday.java
@@ -1,5 +1,30 @@
 package finalexam;
+import java.util.*;
 
-public interface LC01_TwoSum_THSRHoliday {
-    
+public class LC01_TwoSum_THSRHoliday {
+    // 題目邏輯
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{-1, -1}; // 找不到
+    }
+
+    // 測試用 main
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] ans = twoSum(nums, target);
+        System.out.println("Result: [" + ans[0] + ", " + ans[1] + "]");
+    }
 }
+
+/*
+ * Time Complexity: O(n)
+ * 說明：遍歷一次陣列，用 HashMap 查找補數。
+ */
---
+++ finalexam/LC03_NoRepeat_TaipeiMetroTap.java
@@ -0,0 +1,5 @@
+package finalexam;
+
+public class LC03_NoRepeat_TaipeiMetroTap {
+    
+}
